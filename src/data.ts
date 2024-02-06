3a9d27147e8cef2b91ffedfcbf76c4f428ca7437
AFPedreros <felipe.pedreros94@gmail.com>
2024-02-05 20:33:43 -0500

    Add new project card and update about section

---
+++ src/data.ts
@@ -0,0 +1,30 @@
+export const experiences = [
+	{
+		companyName: 'Shambala',
+		companyUrl: 'https://www.shambala.life/',
+		timeRange: 'Sep 2023 - Present',
+		jobTitle: 'Full Stack Developer',
+		description:
+			'Spearheaded the development of the company’s enterprise landing page, laying the groundwork for a stronger and more accessible online presence. I am currently part of a dynamic team transforming an LMS into an interactive platform akin to a social network, featuring discussion groups and an internal chat system to foster user collaboration and communication.'
+	},
+	{
+		companyName: 'Partner',
+		companyUrl: 'https://partnercomunicacion.co/',
+		timeRange: 'Feb 2023 - Sep 2023',
+		jobTitle: 'Front End Developer',
+		description:
+			'Tasked with enhancing client-business interaction through the development of innovative Next.js applications. Notably, I developed an app for automatic invoice redemption via photo submissions, significantly improving operational efficiency, and an internal tool for generating newsletter templates. Additionally, I managed and optimized WordPress sites, elevating user experience and site performance.'
+	}
+];
+
+export const projects = [
+	{
+		title: 'Growth Marketer Portfolio',
+		description:
+			"Developed a minimalist portfolio using Astro and Tailwind CSS, highlighting a growth marketer's expertise with smooth animations and a bento grid for visual content, crafted for impact and speed.",
+
+		image: '/images/project-1.png',
+		tags: ['Astro', 'Tailwind CSS'],
+		siteUrl: 'https://laura-calero-portfolio.vercel.app/'
+	}
+];
