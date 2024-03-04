e942ac7ac65ebb1040dabe373dc319a4409ba6d4
AFPedreros <felipe.pedreros94@gmail.com>
2024-03-04 10:17:59 -0500

    Update SkillsSection and data.ts files

--- src/data.ts
+++ src/data.ts
@@ -1,31 +1,31 @@
 export const experiences = [
 	{
-		companyName: 'Shambala',
-		companyUrl: 'https://www.shambala.life/',
-		timeRange: 'Sep 2023 - Present',
-		jobTitle: 'Full Stack Developer',
+		companyName: "Shambala",
+		companyUrl: "https://www.shambala.life/",
+		timeRange: "Sep 2023 - Present",
+		jobTitle: "Full Stack Developer",
 		description:
-			"Led the creation of our main business webpage, which made us more visible online. Now, I'm working with a team to make our Learning Management System (LMS) more like a social network, adding group chats and messaging to help users work together and talk more easily."
+			"Led the development of our primary business webpage using Next.js for the fro nt-end, significantly enhancing our online visibility. Currently, I'm collaborating with a team to evolve our Learning Management System (LMS) into a more social network-like platform by integrating group chats and messaging features. This enhancement is being achieved through the use of Next.js for the front-end to ensure a seamless user experience and NestJS for the back-end to manage efficient, scalable communication. Our goal is to facilitate easier collaboration and interaction among users.",
 	},
 	{
-		companyName: 'Partner',
-		companyUrl: 'https://partnercomunicacion.co/',
-		timeRange: 'Feb 2023 - Sep 2023',
-		jobTitle: 'Front End Developer',
+		companyName: "Partner",
+		companyUrl: "https://partnercomunicacion.co/",
+		timeRange: "Feb 2023 - Sep 2023",
+		jobTitle: "Front End Developer",
 		description:
-			'Worked on improving how clients and our business interact by building innovative Next.js applications. I developed an app that simplifies invoice processing with photo uploads, which really sped things up. I also built a tool that makes it easy to create newsletter templates. Besides that, I managed and fine-tuned our WordPress websites to enhance the experience for visitors and boost the sites performance.'
-	}
+			"Worked on improving how clients and our business interact by building innovative Next.js applications. I developed an app that simplifies invoice processing with photo uploads, which really sped things up. I also built a tool that makes it easy to create newsletter templates. Besides that, I managed and fine-tuned our WordPress websites to enhance the experience for visitors and boost the sites performance.",
+	},
 ];
 
 export const projects = [
 	{
-		title: 'Growth Marketer Portfolio',
+		title: "Growth Marketer Portfolio",
 		description:
 			"Developed a minimalist portfolio using Astro and Tailwind CSS, highlighting a growth marketer's expertise with smooth animations and a bento grid for visual content, crafted for impact and speed.",
 
-		image: '/images/project-1.png',
-		tags: ['Astro', 'Tailwind CSS'],
-		gitHubUrl: 'https://github.com/AFPedreros/laura-calero-portfolio',
-		siteUrl: 'https://laura-calero-portfolio.vercel.app/'
-	}
+		image: "/images/project-1.png",
+		tags: ["Astro", "Tailwind CSS"],
+		gitHubUrl: "https://github.com/AFPedreros/laura-calero-portfolio",
+		siteUrl: "https://laura-calero-portfolio.vercel.app/",
+	},
 ];
