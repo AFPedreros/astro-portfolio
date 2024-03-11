f87aa70a8d38aae03224d1c7e1b1afb1e0fd00a6
AFPedreros <felipe.pedreros94@gmail.com>
2024-03-11 07:30:19 -0500

    Fix typos and update project details

--- src/data.ts
+++ src/data.ts
@@ -1,31 +1,41 @@
 export const experiences = [
 	{
-		companyName: "Shambala",
-		companyUrl: "https://www.shambala.life/",
-		timeRange: "Sep 2023 - Present",
-		jobTitle: "Full Stack Developer",
+		companyName: 'Shambala',
+		companyUrl: 'https://www.shambala.life/',
+		timeRange: 'Sep 2023 - Present',
+		jobTitle: 'Full Stack Developer',
 		description:
-			"Led the development of our primary business webpage using Next.js for the fro nt-end, significantly enhancing our online visibility. Currently, I'm collaborating with a team to evolve our Learning Management System (LMS) into a more social network-like platform by integrating group chats and messaging features. This enhancement is being achieved through the use of Next.js for the front-end to ensure a seamless user experience and NestJS for the back-end to manage efficient, scalable communication. Our goal is to facilitate easier collaboration and interaction among users.",
+			"Led the development of our primary business webpage using Next.js for the fro nt-end, significantly enhancing our online visibility. Currently, I'm collaborating with a team to evolve our Learning Management System (LMS) into a more social network-like platform by integrating group chats and messaging features. This enhancement is being achieved through the use of Next.js for the front-end to ensure a seamless user experience and NestJS for the back-end to manage efficient, scalable communication. Our goal is to facilitate easier collaboration and interaction among users."
 	},
 	{
-		companyName: "Partner",
-		companyUrl: "https://partnercomunicacion.co/",
-		timeRange: "Feb 2023 - Sep 2023",
-		jobTitle: "Front End Developer",
+		companyName: 'Partner',
+		companyUrl: 'https://partnercomunicacion.co/',
+		timeRange: 'Feb 2023 - Sep 2023',
+		jobTitle: 'Front End Developer',
 		description:
-			"Worked on improving how clients and our business interact by building innovative Next.js applications. I developed an app that simplifies invoice processing with photo uploads, which really sped things up. I also built a tool that makes it easy to create newsletter templates. Besides that, I managed and fine-tuned our WordPress websites to enhance the experience for visitors and boost the sites performance.",
-	},
+			'Worked on improving how clients and our business interact by building innovative Next.js applications. I developed an app that simplifies invoice processing with photo uploads, which really sped things up. I also built a tool that makes it easy to create newsletter templates. Besides that, I managed and fine-tuned our WordPress websites to enhance the experience for visitors and boost the sites performance.'
+	}
 ];
 
 export const projects = [
 	{
-		title: "Growth Marketer Portfolio",
+		title: 'Growth Marketer Portfolio',
 		description:
 			"Developed a minimalist portfolio using Astro and Tailwind CSS, highlighting a growth marketer's expertise with smooth animations and a bento grid for visual content, crafted for impact and speed.",
 
-		image: "/images/project-1.png",
-		tags: ["Astro", "Tailwind CSS"],
-		gitHubUrl: "https://github.com/AFPedreros/laura-calero-portfolio",
-		siteUrl: "https://laura-calero-portfolio.vercel.app/",
+		image: '/images/project-1.png',
+		tags: ['Astro', 'Tailwind CSS'],
+		gitHubUrl: 'https://github.com/AFPedreros/laura-calero-portfolio',
+		siteUrl: 'https://laura-calero-portfolio.vercel.app'
 	},
+	{
+		title: 'TMDB Explorer',
+		description:
+			'Developed &quotCineverse&quot, a Next.js web application offering an engaging video browsing experience. Users can effortlessly register and explore a curated selection of movies and series, with infinite scrolling and dynamic categorization, all integrated with the TMDB API for real-time data. Designed with Tailwind CSS for a sleek look, and Clerk for secure authentication, this project delivers both high performance and immersive user interactions.',
+
+		image: '/images/project-2.png',
+		tags: ['Next.js', 'Tailwind CSS', 'Framer Motion', 'Clerk'],
+		gitHubUrl: 'https://github.com/AFPedreros/video-app',
+		siteUrl: 'cineverse-beta.vercel.app'
+	}
 ];
